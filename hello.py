d0e5518ba1afbd23337d22a3795b9177ae7f679e
akhiakter25556 <akhiakterherpower80@gmail.com>
2026-05-02 00:57:01 +0600

    first commit

---
+++ hello.py
@@ -0,0 +1,74 @@
+# print("Hello, Python!")
+
+
+# name = "Alice"
+# age = 30
+# print(f"{name} is {age} years old.")
+
+
+# name = input("Enter your name: ")
+# print("Hello,", name)
+
+
+
+# age  = int(input("Enter your age: "))
+
+# next_year = age + 1
+# print("Next year, you will be:", next_year )
+
+
+# a = int(input("Enter first number: 3"))
+# b = int(input("Enter second number: 7 "))
+# print(" sum is:", a + b)
+
+
+
+# name = "Akhi" 
+# age = 20
+# is_student = True
+# print( name, age, is_student)
+
+# a = 6
+# b = 4
+# print(a + b)
+# print(a - b)
+# print(a * b)    
+# print(a / b)
+# print(a // b)
+# print(a % b)
+# print(a ** b)
+# name = input("Enter your name: ")
+# print("Hello,", name)
+
+# age = int(input("Enter your age: "))
+# if age < 18:
+#     print(" you are  adult")
+# else:
+#     print("you are minor")
+
+# for i in range(5):
+#     print (i)
+#     i += 1  
+
+#     def greet(name):
+#         return "Hello " + name
+#     print(greet("Alice"))   
+# # for loop
+# for i in range(5):
+# print(i)
+
+# # while loop
+# i = 0
+# while i < 5:
+#     print(i)
+#     i += 1  
+
+
+fruits = ["apple", "banana", "cherry"]
+print(fruits[0])  # Output: apple
+
+person = {"name": "Alice", "age": 30}
+print(person["name"])  # Output: Alice
+print(person["age"])   # Output: 30
+
+
